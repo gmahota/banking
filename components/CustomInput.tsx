a3d6888d344b00de2a26e3572393e000efd85163
gmahota <guimaraesmahota@gmail.com>
2024-06-28 22:04:55 +0200

    create login and logout routes - first commit

---
+++ components/CustomInput.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from './ui/form'
+
+import { Input } from "@/components/ui/input"
+
+import {Control, FieldPath} from 'react-hook-form'
+
+import { z } from "zod"
+
+import { authFormSchema } from "@/lib/utils"
+
+interface CustomImport{
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: FieldPath<z.infer<typeof authFormSchema>>,
+    label:string,
+    placeholder:string
+}
+
+const CustomInput = ({ control, name, label, placeholder }:CustomImport) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <div className='form-item'>
+                    <FormLabel className='form-label'>
+                        {label}
+                    </FormLabel>
+                    <div className='flex w-full flex-col'>
+                        <FormControl>
+                            <Input
+                                placeholder={placeholder}
+                                className='input-class'
+                                type={name==='password'?'password':'text'}
+                                {...field} />
+                        </FormControl>
+                        <FormMessage className='form-message mt-2' />
+                    </div>
+                </div>
+            )}
+        />
+    )
+}
+
+export default CustomInput
